adfa13230eb82c4bd78ddc9cf5e0f1f77c1cbbea
yosefther <yousefthair9@gmail.com>
2025-08-23 15:11:32 +0300

    Refactor event handling and screen updating methods for improved readability

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,15 +13,21 @@ class AlienInvasion:
         self.clock = pygame.time.Clock()
         self.bg_color = (Set.bg_color)
         self.ship = Ship(self)
-    
-    def run_game(self):
-        while True:
-            for event in pygame.event.get():
+ 
+    def _check_events(self):
+        for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                   sys.exit()
-            pygame.display.flip()
+
+    def _update_screen(self):
             self.screen.fill(self.bg_color)
             self.ship.blitme()
+            pygame.display.flip()
+
+    def run_game(self):
+        while True:
+            self._check_events()
+            self._update_screen()
             self.clock.tick(60)
 
 if __name__ == '__main__':
