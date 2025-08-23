0f8ea1c0080417ec99c72621407220f01ad6b435
yosefther <yousefthair9@gmail.com>
2025-08-23 15:21:39 +0300

    Update background color in Settings class for improved visual contrast

--- settings.py
+++ settings.py
@@ -1,5 +1,8 @@
+import pygame
+import sys
 class Settings:
     def __init__(self):
         self.screen_width = 1200
         self.screen_height = 800 
-        self.bg_color = (230,230,230)
+        self.bg_color = (153,0,0)
+
