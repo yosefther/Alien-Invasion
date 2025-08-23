86b5d8688c6a167a1956b80d5c4b2ed3a0fa7ed7
yosefther <yousefthair9@gmail.com>
2025-08-23 16:10:39 +0300

    Implement ship movement controls and update method for player character

--- ship.py
+++ ship.py
@@ -5,8 +5,17 @@ class Ship:
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
         self.image = pygame.image.load('assets\ship1.bmp')
+        self.image = pygame.transform.scale(self.image,(100, 100))
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
+        self.moving_right = False
+        self.moving_left = False 
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
+
+    def update(self):
+        if self.moving_right:
+            self.rect.x+=2
+        elif self.moving_left:
+            self.rect.x-=2
