64753709d879ef455f80a71f188938baafc383c3
yosefther <yousefthair9@gmail.com>
2025-08-23 16:11:07 +0300

    Update background color in Settings class for improved visual contrast

--- settings.py
+++ settings.py
@@ -4,5 +4,5 @@ class Settings:
     def __init__(self):
         self.screen_width = 1200
         self.screen_height = 800 
-        self.bg_color = (153,0,0)
+        self.bg_color = (51,255,255)
 
