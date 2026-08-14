471549a142c39c6c01c444df80af770db3328ba9
yosefther <yousefthair9@gmail.com>
2026-08-14 12:47:00 -0500

    Track session score lives and level

---
+++ game_stats.py
@@ -0,0 +1,14 @@
+class GameStats:
+    """Track values that change during a play session."""
+
+    def __init__(self, ai_game):
+        self.settings = ai_game.settings
+        self.high_score = 0
+        self.game_active = False
+        self.paused = False
+        self.reset_stats()
+
+    def reset_stats(self):
+        self.ships_left = self.settings.ship_limit
+        self.score = 0
+        self.level = 1
